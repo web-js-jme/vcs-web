e2a37c53d4fa403df9c11c4e535d99a009e6844e
DzeiEm <julija.melesina@gmail.com>
2024-03-04 19:51:23 +0200

    calcu

---
+++ JS/skaiciuotuvas/scripts.js
@@ -0,0 +1,64 @@
+let firstNumber = 0;
+let second_number = 0;
+let action = '+';
+let answer = 0;
+
+
+let input = document.getElementById ('calc-input');
+let calculationSpan = document.getElementById('calculation');
+
+let histiry = [];
+
+function onNumberClick (number) {
+    input.value += number;
+}
+
+
+function onActionClick (clickedAction) {
+    input.value += '' + clickedAction + '';
+    action = clickedAction;
+}
+
+function onActionClick(clickedAction) {
+    input.value += '' + clickedAction + '';
+    action = clickedAction;
+}
+
+function onCountClick() {
+    input.value += '' + clickedAction + '';
+    firstNumber = parseInt(splitted[0])
+    action = splitted[1];
+    second_number = parseInt(splitted[2]);
+
+    calculateAnswer();
+    input.value = answer;
+}
+
+function calculateAnswer() {
+    switch (action) {
+        case '+': answer = firstNumber + second_number; break;
+        case '-': answer = firstNumber - second_number; break;
+        case 'x': answer = firstNumber * second_number; break;
+        case '/': answer = firstNumber / second_number; break;
+    }
+
+    function onCleanClick() {
+        firstNumber = 0;
+        second_number = 0;
+        action = '+';
+        answer = 0;
+        input.value = '';
+        calculationSpan.innerHTML = '';
+    }
+}
+
+
+function addToHistory() {
+    let histotyItem = {
+        firstNumber,
+        action,
+        secondaryNumber,
+        answer
+    };
+    histiry.push(histotyItem);
+}
